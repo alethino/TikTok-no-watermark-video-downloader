fd216f3b35a3be64088844a05cca5fcb83ca94cf
alethino <gneo0194@gmail.com>
2025-09-02 21:50:58 +0300

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -1,7 +1,7 @@
 
 ### contact me
-**Contact for api :** [https://t.me/Neo_Galaxy](https://t.me/Neo_Galaxy)
-## telegram: @Neo_Galaxy
+**Contact for api :** [https://t.me/alethino](https://t.me/alethino)
+## telegram: @alethino
 import utils
 class Account:
     def __init__(self) -> None:
